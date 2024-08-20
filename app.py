2d9d8210325a0c6e6f5f5d1837db83d842295d2d
erq1000 <64738320+erq1000@users.noreply.github.com>
2024-08-20 20:52:46 +0200

    add error catching

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from dotenv import load_dotenv
 
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(error=404, text=str(e)), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return jsonify(error=500, text="Internal server error, we're working to fix it!"), 500
 
 def get_port():
     return int(os.getenv('PORT', 5000))
